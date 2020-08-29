71120992829b974519797d08a72dcf4075cf9b5f
Reynoso746678 <47166791+Reynoso746678@users.noreply.github.com>
2020-08-28 17:39:15 -0700

    Update

--- WebScraperBlackfriday/WebScraperBlackfriday.py
+++ WebScraperBlackfriday/WebScraperBlackfriday.py
@@ -3,13 +3,18 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
+#URL of Amazon item
 URL = 'https://www.amazon.com/dp/B00W2D806Y/?coliid=I25HCE64XGIDZ9&colid=3R7CIZSP5HK6T&psc=1&ref_=lv_ov_lig_dp_it'
 
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'}
 
+#Check if price has fallen
 def check_price():
+    
+    #Combine request(s)
     page = requests.get(URL, headers=headers)
-
+    
+    #Amazon page request(s)
     soup = BeautifulSoup(page.content, 'lxml')
     title = soup.find(id="productTitle").get_text()
     price = soup.find(id="priceblock_ourprice").get_text()
@@ -18,19 +23,24 @@ def check_price():
     if(converted_price < 39):
         send_mail()
 
+#Send Email 
 def send_mail():
+
+    #Email touch
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
+    #Depending on your 2 step verification process through a google account
     server.login('gmail', 'pass')
-
-    subject = 'Price fell down!'
+    
+    #Components of Message
+    subject = 'Yo! Its finally fallen'
     body = 'Check the amazon link https://www.amazon.com/dp/B00W2D806Y/?coliid=I25HCE64XGIDZ9&colid=3R7CIZSP5HK6T&psc=1&ref_=lv_ov_lig_dp_it'
-
     msg = f"Subject: {subject}\n\n{body}"
-
+    
+    #From (email address) & to (Email Address)
     server.sendmail(
         'from',
         'to',
@@ -38,7 +48,9 @@ def send_mail():
     )
     server.quit()
 
+#Time to recheck price
 while(True):
     check_price()
+
     # 24 hour 
     time.sleep(82400)
